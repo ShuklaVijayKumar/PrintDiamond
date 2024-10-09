ecd8ce4b75f33ddbd7e073f8890de06c42faa65c
ShuklaVijayKumar <shukla.vijay@outlook.com>
2024-10-09 15:40:32 +0100

    Add files via upload

---
+++ DiamondTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System;
+using System.IO;
+
+[TestFixture]
+public class DiamondTests
+{
+    private StringWriter consoleOutput;
+
+    [SetUp]
+    public void SetUp()
+    {
+        consoleOutput = new StringWriter();
+        Console.SetOut(consoleOutput);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        consoleOutput.Dispose();
+    }
+
+    [Test]
+    public void TestPrintDiamondWithA()
+    {
+        Program.PrintDiamond('A');
+        string expectedOutput = "A\r\n"; // Output with trailing underscores for visualization
+        ClassicAssert.AreEqual(expectedOutput, consoleOutput.ToString());
+    }
+
+    [Test]
+    public void TestPrintDiamondWithB()
+    {
+        Program.PrintDiamond('B');
+        string expectedOutput = "_A_\r\nB_B\r\n_A_\r\n";
+        ClassicAssert.AreEqual(expectedOutput, consoleOutput.ToString());
+    }
+
+    [Test]
+    public void TestPrintDiamondWithC()
+    {
+        Program.PrintDiamond('C');
+        string expectedOutput = "__A__\r\n_B_B_\r\nC___C\r\n_B_B_\r\n__A__\r\n";
+        ClassicAssert.AreEqual(expectedOutput, consoleOutput.ToString());
+    }
+
+    [Test]
+    public void TestPrintDiamondWithB_Failing() // Deliberate failed
+    {
+        // Intentionally provide an incorrect expected output
+        Program.PrintDiamond('B');
+        string expectedOutput = "_A__\r\nB B\r\n_A__\r\n"; // Missing underscores to visualize spaces correctly
+        ClassicAssert.AreNotEqual(expectedOutput, consoleOutput.ToString());
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a character: ");
+        char inputChar = char.ToUpper(Console.ReadLine()[0]);
+        PrintDiamond(inputChar);
+    }
+
+    public static void PrintDiamond(char targetChar)
+    {
+        int size = targetChar - 'A';
+
+        // Build the diamond lines
+        for (int i = 0; i <= size; i++)
+        {
+            Console.WriteLine(GenerateLine(i, size));
+        }
+
+        for (int i = size - 1; i >= 0; i--)
+        {
+            Console.WriteLine(GenerateLine(i, size));
+        }
+    }
+
+    private static string GenerateLine(int lineIndex, int totalSize)
+    {
+        char letter = (char)('A' + lineIndex);
+        int outerSpaces = totalSize - lineIndex;
+        int innerSpaces = lineIndex > 0 ? 2 * lineIndex - 1 : 0;
+
+        // Create the line using underscores to visualize spaces
+        string line = new string('_', outerSpaces) + letter;
+        if (innerSpaces > 0)
+        {
+            line += new string('_', innerSpaces) + letter;
+        }
+
+        return line.PadRight(2 * totalSize + 1, '_');
+    }
+}
